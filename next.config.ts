2f5196adb9fbff49663137160e62de01538eecc3
waqaskhan333421 <wk333421@gmail.com>
2026-04-03 11:12:16 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "standalone",
+//   /* config options here */
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   reactStrictMode: false,
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export",      // Change this from 'standalone' to 'export'
+  basePath: "/Portfolio", // Required so CSS/JS files load from the right folder
+  images: {
+    unoptimized: true,   // GitHub Pages doesn't support Next.js Image Optimization
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
